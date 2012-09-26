d2b8005b61c68628e41efcdcad64c5f955e857e0
p4plus2 <p4plus2@gmail.com>
2012-09-26 02:00:17 -0700

    Added .gitattributes file to make the version script not effected by the changes of version.h

--- version.h
+++ version.h
@@ -2,11 +2,11 @@
 void display_version_dialog()
 {
 	QMessageBox version_info;
-	version_info.setText(QString("The current version is: v12-WIP").leftJustified(100));
+	version_info.setText(QString("The current version is: v13").leftJustified(100));
 	version_info.setWindowTitle("Version info");
 	version_info.setDetailedText(
 				     "The current build branch is: master\n"
-				     "The current commit sha1 is: 4f48a466363e20ca9998d577d1a5db9b04799ad0\n"
+				     "The current commit sha1 is: b09d64c150c975635351198f9872ba32ae360038\n"
 				     );
 	version_info.setWindowFlags(version_info.windowFlags() ^ Qt::WindowCloseButtonHint);
 	version_info.exec();
